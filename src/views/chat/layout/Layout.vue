b569548cf6199daf44cbf54a82578784161f7e29
fanfan2289 <110893304+fanfan2289@users.noreply.github.com>
2023-04-27 00:46:46 +0800

    Update Layout.vue

--- src/views/chat/layout/Layout.vue
+++ src/views/chat/layout/Layout.vue
@@ -38,13 +38,13 @@ const getContainerClass = computed(() => {
     <div class="h-full overflow-hidden" :class="getMobileClass">
       <NLayout class="z-40 transition" :class="getContainerClass" has-sider>
         <Sider />
+				<img src="./test.jpg" alt="your-image-alt-text">
         <NLayoutContent class="h-full">
           <RouterView v-slot="{ Component, route }">
             <component :is="Component" :key="route.fullPath" />
           </RouterView>
         </NLayoutContent>
       </NLayout>
-		  <img src="./test.jpg" alt="your-image-alt-text">
     </div>
     <Permission :visible="needPermission" />
   </div>
